d6bc3461881fb42b3e55ce21a347c3a88ca83057
Mika972 <mikl94v@hotmail.com>
2019-07-31 11:51:27 +0200

    Create egalite.js

---
+++ hello/egalite.js
@@ -0,0 +1,22 @@
+/Eality*******
+var x = 7, y = 7;
+if (x == y) {
+  console.log("x=7 est égal à y=7");
+}
+
+/*A tester en retirant les slash commentaires du bas et en mettant la première
+condition du haut en commentaire*/
+/*
+x = "4";
+if (x == Y) {
+console.log("x='4' est égal à y=7");
+}
+*/
+
+//Stricte Egalité*******
+if (x === y)  {
+  console.log("Strict: x='4' est égal à y=4");
+}
+else {
+  console.log("Strict: x='4' n'est pas égal à y=4");
+}
