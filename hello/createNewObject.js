b8f2d639307caf3d36b4ef6f91ae6bd38b93853a
Mika972 <mikl94v@hotmail.com>
2019-08-12 00:04:17 +0200

    Update createNewObject.js

--- hello/createNewObject.js
+++ hello/createNewObject.js
@@ -3,7 +3,16 @@
 // Object creation
 var company = new Object();
 company.name = "facebook";
-company.ceo.firstName = "Mark"; // va provoquer une erreur, mais uniquement la ligne au-dessus sera bon
-
+company.ceo.firstName = "Mark"; // va provoquer une erreur, mais uniquement la ligne 
+                                // au-dessus sera bon.
 console.log(company);
+
+/*
+* Si on déclare company.ceo comme un objet, alors il apparait bien dans la console
+*/
+
+company.ceo = new Object();
+company.ceo.firstName = "Mark";
+
+console.log(compay);
 
