66c6645ee51fdae0a9c479adad269d69c8f7d356
Mika972 <mikl94v@hotmail.com>
2019-08-11 23:55:37 +0200

    Update createNewObject.js

--- hello/createNewObject.js
+++ hello/createNewObject.js
@@ -1,1 +1,9 @@
+'use strict'
+
+// Object creation
+var company = new Object();
+company.name = "facebook";
+company.ceo.firstName = "Mark"; // va provoquer une erreur, mais uniquement la ligne au-dessus sera bon
+
+console.log(company);
 
