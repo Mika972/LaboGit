39fd4fe853e890e964cacf97a62c96df9fc982f9
Mika972 <mikl94v@hotmail.com>
2019-08-16 00:43:22 +0200

    Update fonctionExplications.js

--- hello/fonctionExplications.js
+++ hello/fonctionExplications.js
@@ -9,5 +9,6 @@ function multiply(x, y) {
 console.log(multiply(3, 7));// Cela donne 21 dans la console
 
 /*Autre exemple, ajout de.version, afin de renseigner la version du programme*/
-
-
+multiply.version = "v.1.0.0";
+console.log(multiply.toString());
+console.log(multiply.version);
