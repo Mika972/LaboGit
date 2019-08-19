694ddc51ee593151ca60d0aa8199d609a10d29d8
Mika972 <mikl94v@hotmail.com>
2019-08-19 15:59:56 +0200

    Update passingVarvsValues1.js

--- hello/passingVarvsValues1.js
+++ hello/passingVarvsValues1.js
@@ -1,1 +1,16 @@
+'use strict'
 
+var a = 7;
+var b = a;
+b = 5;
+
+console.log("La valeur de a est " + a);
+console.log("La valeur de b est " + b);
+
+/*-2 ème partie avec un object cette fois***********/
+var c = {x: 7};
+var d = c;
+var e = d.x;
+console.log("La valeur de c est " + c);
+console.log("La valeur de d est " + d);
+console.log("La valeur de d.x est " + e);
