e588ef08bfd0533d48d6c50fd56c0d37a85ac711
Mika972 <mikl94v@hotmail.com>
2019-08-19 22:04:30 +0200

    Update passingVarvsValues2.js

--- hello/passingVarvsValues2.js
+++ hello/passingVarvsValues2.js
@@ -10,3 +10,35 @@ b.x = 5;
 console.log("after b.x update:");
 console.log(a);
 console.log(b);
+
+/**Un autre exemple, mettre les lignes ci-dessus en commentaires pour voir ce qui se passe en dessou*/
+function changePrimitive(primValue) {
+  console.log("in changePrimitive...");
+  console.log("before:");
+  console.log(primValue);
+  
+  primValue = 5;
+  console.log("after:");
+  console.log(primValue);
+}
+
+var value = 7;
+changePrimitive(value); // primValue = value
+console.log("after changePrimitive, orig value:");
+console.log(value);
+  
+/**Autre exemple, avec cette fois changement d'objet*/
+function changeObject(objValue) {
+  console.log("in changeObject...");
+  console.log("before:");
+  console.log(objValue);
+  
+  objValue.x = 5;
+  console.log("after:");
+  console.log(objValue);
+}
+
+value = {x: 7};
+changeObject(value);
+console.log("after changeObject, orig value:");
+console.log(value);
