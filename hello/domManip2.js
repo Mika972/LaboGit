47bd9766a2e62a3f0fddccf5c0e488158d237df0
Mika972 <mikl94v@hotmail.com>
2019-09-02 10:13:15 +0200

    Update domManip2.js

--- hello/domManip2.js
+++ hello/domManip2.js
@@ -5,11 +5,24 @@ function sayHello() {
 console.log(document.getElementById("name").value;
 }
 
-/****Autre exemple(Il faudra penser à placer la fonction du dessus en 
-commentaire)*****/
+/***
+*Autre exemple(Il faudra penser à placer la fonction du dessus en 
+commentaire)*
+****/
 function sayHello() {
   var name = document.getElementById("name").value;
-  var message = "Hello " + name + "!" ;
+  var message = "Hello " + name + "!";
   //Cela signifie que dans la page web,  l'élémenet ID "content", affichera le message
   document.getElementById("content").textContent = message;
 }
+/*
+* Cette fois on veut que le contenu de la balise h2 soit modifié
+* Il faudra utiliser innerHTML(Penser à mettre les fonction ci-dessus en commentaire)
+*/
+function sayHello() {
+  var name = document.getElementById("name").value;
+  var message = "<h2>Hello " + name + "!</h2>";
+  //Cela signifie que dans la page web,  l'élémenet ID "content", affichera le message
+  document.getElementById("content").innerHTML = message;
+}
+  
