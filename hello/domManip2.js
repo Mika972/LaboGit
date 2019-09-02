7802419a10bc001e3e8f60dd2ab6b3d35b1b3011
Mika972 <mikl94v@hotmail.com>
2019-09-02 08:29:13 +0200

    Update domManip2.js

--- hello/domManip2.js
+++ hello/domManip2.js
@@ -1,1 +1,15 @@
+'use strict'
+/* Manipulation du Dom, suite*/
+//Value permet de récupérer la valeur dans la case de la page HTML
+function sayHello() {
+console.log(document.getElementById("name").value;
+}
 
+/****Autre exemple(Il faudra penser à placer la fonction du dessus en 
+commentaire)*****/
+function sayHello() {
+  var name = document.getElementById("name").value;
+  var message = "Hello " + name + "!" ;
+  
+  document.getElementById("content").textContent = message;
+}
