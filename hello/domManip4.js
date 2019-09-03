a49678d1077b9ac5b5bff807f114c707fb4308d1
Mika972 <mikl94v@hotmail.com>
2019-09-04 00:03:28 +0200

    Create domManip4.js

---
+++ hello/domManip4.js
@@ -0,0 +1,27 @@
+'use strict'
+/*
+* On peut faire encore mieux. Faire l'équivalent de domManipulation3 & domManipulation2,
+* avec fonction4 dans une seule fonction !!
+* Il faut utiliser avec <button>Say it</button> et ne laisser que domManipulation4.js actif
+* dans HTML
+*/
+document.addEventListener("DOMContentLoaded",
+  function(evenement) {
+    function sayHello(evenement) { //fonction4
+      /*console.log(this);*/ // this renvoie à sayHello()
+      this.textContent = "Said it !";//Il n'affichera pas "Said it !"
+      var name = document.getElementById("name").value;
+      var message = "<h2>Hello " + name + " !</h2>";
+    //Cela signifie que dans la page web,  l'élémenet ID "content", affichera le message
+      document.getElementById("content").innerHTML = message;
+
+      if (name === "student") {
+        var title = document.querySelector("#title").textContent;//# car c'est une class
+        title += " & lovin' it!";
+        document.querySelector("#title").textContent = title;
+      }
+    }
+    /*unobtrusive event binding*/
+    document.querySelector("button").addEventListener("click", sayHello);
+  }
+);
