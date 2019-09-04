e58e892e37c06782837a6a9dc91bc26ed39f1d6a
Mika972 <mikl94v@hotmail.com>
2019-09-04 22:16:15 +0200

    Create eventArgument.js

---
+++ hello/eventArgument.js
@@ -0,0 +1,20 @@
+'use strict'
+
+/**
+* event en argument dans une fonction, sert à capter les évènements du DOM (page HTML)
+* afin de les exploiter dans le corp de la fonction, ou des fonctions
+* Dans le HTML, n'utiliser que eventArgument.js
+**/
+
+document.addEventListener("DOMContentLoaded",
+  function(evenement) { //evenement n'est qu'un argument qui aurait pu être remplacé par n'importe quel nom
+
+    document.querySelector("body").addEventListener("mousemove",
+      function(evenement) {
+        console.log(event.clientX);
+        console.log(event.clientY);
+      }
+    );
+
+  }
+);
