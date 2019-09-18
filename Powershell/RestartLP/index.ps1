e7ebcdaf6d6843ffaee3119ff59032047df44b2a
Mika972 <mikl94v@hotmail.com>
2019-09-18 15:06:44 +0200

    Update index.ps1

--- Powershell/RestartLP/index.ps1
+++ Powershell/RestartLP/index.ps1
@@ -1,5 +1,5 @@
 ####
-# RestartLP
+# RestartLP 17/09/2019
 ####
 
 [void][reflection.assembly]::Load('System.Windows.Forms, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089')
@@ -12,5 +12,15 @@ $form = New-Object System.Windows.Forms.Form
 $label_postes = New-Object System.Windows.Forms.Label
 $label_messages = New-Object System.Windows.Forms.Label
 $bouton_Start = New-Object System.Windows.Forms.Button
+$checkbox_Pythagore = New-Object System.Windows.Forms.checkbox
 $checkbox_Pascal = New-Object System.Windows.Forms.checkbox
 $checkbox_Einstein = New-Object System.Windows.Forms.checkbox
+$checkbox_Darwin = New-Object System.Windows.Forms.checkbox
+$checkbox_Curie = New-Object System.Windows.Forms.checkbox
+$checkbox_Descartes = New-Object System.Windows.Forms.checkbox
+$checkbox_Archimede = New-Object System.Windows.Forms.checkbox
+$checkbox_Newton = New-Object System.Windows.Forms.checkbox
+$checkbox_Copernic = New-Object System.Windows.Forms.checkbox
+$checkbox_Kepler = New-Object System.Windows.Forms.checkbox
+#
+#
