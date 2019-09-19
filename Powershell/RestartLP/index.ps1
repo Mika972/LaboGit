32636835679c8fcbd25f79fc6e133ee741efa9a9
Mika972 <mikl94v@hotmail.com>
2019-09-19 11:52:17 +0200

    Update index.ps1

--- Powershell/RestartLP/index.ps1
+++ Powershell/RestartLP/index.ps1
@@ -1,17 +1,18 @@
-####
-# RestartLP 17/09/2019
-####
+####=============####
+#    RestartLPV1    # 17/09/2019
+####=============####
 
 [void][reflection.assembly]::Load('System.Windows.Forms, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089')
 [void][reflection.assembly]::Load('System.Data, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089')
 [void][reflection.assembly]::Load('System.Drawing, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a')
 [void][reflection.assembly]::Load('System.DirectoryServices, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a')
 [void][reflection.assembly]::Load('System.ServiceProcess, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a')
+## Déclarations des variables
 ##
 $form = New-Object System.Windows.Forms.Form
 $label_postes = New-Object System.Windows.Forms.Label
 $label_messages = New-Object System.Windows.Forms.Label
-$bouton_Start = New-Object System.Windows.Forms.Button
+$bouton_ReStart = New-Object System.Windows.Forms.Button
 $checkbox_Pythagore = New-Object System.Windows.Forms.checkbox
 $checkbox_Pascal = New-Object System.Windows.Forms.checkbox
 $checkbox_Einstein = New-Object System.Windows.Forms.checkbox
@@ -23,4 +24,21 @@ $checkbox_Newton = New-Object System.Windows.Forms.checkbox
 $checkbox_Copernic = New-Object System.Windows.Forms.checkbox
 $checkbox_Kepler = New-Object System.Windows.Forms.checkbox
 #
+# Form de RestartLP
 #
+$form.ClientSize = New-Object System.Drawing.Size(450, 450)
+$form.Controls.Add($label_postes)
+$form.Controls.Add($label_messages)
+$form.Controls.Add($bouton_ReStart)
+$form.Controls.Add($bouton_Reset )
+$form.Controls.Add($checkbox_Pascal)
+$form.Controls.Add($checkbox_Einstein)
+$form.Name = 'RestartLPV1'
+$form.Text = 'RestartLPV1'
+$form.Opacity = 0.97 #Pour l'opacity comme en CSS
+#
+# 
+#
+
+#=============================================================================================================================#
+$form.ShowDialog()
