7a4729306e3c3ce97d9c534ae1df61a9201da0ee
Mika972 <mikl94v@hotmail.com>
2019-09-23 13:01:53 +0200

    Update utilForjson.js

--- AJAX/JSON/utilForjson.js
+++ AJAX/JSON/utilForjson.js
@@ -1,1 +1,35 @@
+'use strict';
 
+(function(global) {
+
+// Set up a namespace for our utility
+var ajaxUtils = {};
+
+// Returns an HTTP request object
+function getRequestObject() {
+ if (window.XMLHttpRequest) {
+  return (new XMLHttpRequest());
+  }
+  else if (window.ActiveXObject)  {
+    // For very old IE browsers (optional)
+    return (new ActiveXObject("Microsoft.XMLHTTP"));
+  }
+  else  {
+    global.alert("Ajax is not supported !");
+    return(null);
+  }
+ }
+
+// Makes an Ajax Get request to 'requestUrl'
+ajaxUtils.sendGetRequest = 
+  function(requestUrl, responseHandler, isJsonResponse) {
+    var request = getResquestObject();
+    request.onreadystatechange =
+      function()  {
+        handleResponse(request,
+                       responseHandler,
+                       isJsonResponse);
+      };
+}
+  
+});
