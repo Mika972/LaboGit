308ff25b6eedd690c47367da29ceadd46c7378ff
Mika972 <mikl94v@hotmail.com>
2019-09-26 15:28:52 +0200

    Update index.ps1

--- Powershell/RestartLP/index.ps1
+++ Powershell/RestartLP/index.ps1
@@ -1,5 +1,5 @@
 ####=============####
-#    RestartLPV1    # 17/09/2019
+#    RestartLPV1    #
 ####=============####
 
 [void][reflection.assembly]::Load('System.Windows.Forms, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089')
@@ -11,6 +11,7 @@
 ##
 $form = New-Object System.Windows.Forms.Form
 $label_postes = New-Object System.Windows.Forms.Label
+$label_Salles = New-Object System.Windows.Forms.Label
 $label_messages = New-Object System.Windows.Forms.Label
 $bouton_ReStart = New-Object System.Windows.Forms.Button
 $bouton_Reset = New-Object System.Windows.Forms.Button
@@ -29,17 +30,26 @@ $checkbox_Kepler = New-Object System.Windows.Forms.checkbox
 #
 $form.ClientSize = New-Object System.Drawing.Size(450, 450)
 $form.Controls.Add($label_postes)
+$form.Controls.Add($label_Salles)
 $form.Controls.Add($label_messages)
 $form.Controls.Add($bouton_ReStart)
-$form.Controls.Add($bouton_Reset )
+$form.Controls.Add($bouton_Reset)
+$form.Controls.Add($Pythagore)
 $form.Controls.Add($checkbox_Pascal)
 $form.Controls.Add($checkbox_Einstein)
+$form.Controls.Add($checkbox_Darwin)
+$form.Controls.Add($checkbox_Curie)
+$form.Controls.Add($checkbox_Descartes)
+$form.Controls.Add($checkbox_Archimede)
+$form.Controls.Add($checkbox_Newton)
+$form.Controls.Add($checkbox_Copernic)
+$form.Controls.Add($checkbox_Kepler)
 $form.Name = 'RestartLPV1'
 $form.Text = 'RestartLPV1'
 $form.Opacity = 0.97 #Pour l'opacity comme en CSS
 #
-# 
+# Label salles
 #
-
+$label_Salles
 #=============================================================================================================================#
 $form.ShowDialog()
