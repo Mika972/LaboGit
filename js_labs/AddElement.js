39a3cb34d02483933234a0b18ed4ca23193f9e28
Mika972 <mikl94v@hotmail.com>
2019-11-26 22:35:53 +0100

    Update AddElement.js

--- js_labs/AddElement.js
+++ js_labs/AddElement.js
@@ -3,19 +3,17 @@
 /*Variable globale*/
 var partit = document.createElement("p");
 
-
-
-/*Test du contenu de div visible dans la console*********/
-partit.append("Du texte à tester");                   /**/
-                                                      /**/
-console.log(partit.textContent);                      /**/
-/********************************************************/
-
-
-
-
 //Chargement d'une chaine de caractère dans la variable écrit
 var ecrit = partit.innerHTML = "Enfin du texte visible dans une page html";
 
 //Ajouts dans le DOM d'un nouvel élément et chargement de l'écrit dans ce nouvel élément
 document.body.appendChild(partit, ecrit);
+
+/*Test du contenu de div visible dans la console*********/
+/*ATTENTION, dans la situation actuelle, le texte       */
+/*ci-dessous apparaitra, car avant, "p" a été crée ET   */
+/*déclarer dans le DOM                                  */
+partit.append(" Du texte à tester avec append");      /**/
+                                                      /**/
+console.log(partit.textContent);                      /**/
+/********************************************************/
