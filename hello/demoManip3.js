9eccc4e01c44b3008e68249313e25eb86423084e
Mika972 <mikl94v@hotmail.com>
2021-02-19 15:45:39 +0100

    Delete hello directory

--- hello/demoManip3.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-/**
-* Attention, pour que cela fonctionne, il faut que dans HTML <button></button>. SANS onclick !
-* "unobtrusive event binding" = Une autre manière plus discrète de déclarer et créer un évènement en javascript
-**/
-
-/*unobtrusive event binding*/
-document.querySelector("button").addEventListener("click", sayHello);//Ecouteurs ?
--- hello/hello1.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-
-var x = "Hello world !";
